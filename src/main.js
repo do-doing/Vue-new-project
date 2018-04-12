0558ce60287f241315599995cf868b7171caf05b
do-doing <751276089@qq.com>
2018-04-12 22:13:59 +0800

    打包

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
+require('./assets/css/base.css');
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
